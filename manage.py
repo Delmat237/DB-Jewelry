222aa7453e41db181546c531ec45748849f7a4b9
Delmat Leonel AZANGUE <142000059+Delmat237@users.noreply.github.com>
2025-06-30 22:50:15 +0000

    superuser

--- manage.py
+++ manage.py
@@ -15,22 +15,17 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-
-    # Créer le superutilisateur uniquement si on exécute le serveur
-    if len(sys.argv) > 1 and sys.argv[1] == "runserver":
-        try:
-            from django.contrib.auth import get_user_model
-            import django
-            django.setup()
-            User = get_user_model()
-            if not User.objects.filter(username="admin").exists():
-                User.objects.create_superuser("admin", "azangueleonel9@gmail.com", "adminpass")
-                print("✅ Superuser créé automatiquement.")
-        except Exception as e:
-            print("⚠️ Erreur lors de la création du superutilisateur :", e)
-
     execute_from_command_line(sys.argv)
 
 
 if __name__ == '__main__':
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'db_jewelry_backend.settings')
+    import django
+    django.setup()
+
+    from django.contrib.auth import get_user_model
+    User = get_user_model()
+    if not User.objects.filter(username="admin").exists():
+        User.objects.create_superuser("admin", "azangueleonel9@gmail.com", "adminpass")
+        print("✅ Superuser créé automatiquement.")
     main()
